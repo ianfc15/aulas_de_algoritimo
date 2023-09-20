8d5e3a404c91ac831fc8fd67e6548193be30121f
ianfc15 <ianfcampos5@gmail.com>
2023-09-19 21:10:21 -0300

    Vetor

---
+++ 19.09.2023/Vetor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    double notas[4];
+    char nomeAluno[20];
+
+    printf("Insira o nome do aluno:\n");
+    scanf("%s", &nomeAluno);
+    printf("Nome do aluno: %s\n");
+
+
+    return 0;
+}
